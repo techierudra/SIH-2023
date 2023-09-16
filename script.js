e60e9febc03bca7245d08a7729d49b0354c679ee
RUDRA GARAI <103626421+techierudra@users.noreply.github.com>
2023-09-16 14:59:27 +0530

    renamed

---
+++ script.js
@@ -0,0 +1,21 @@
+// Get references to the form and its elements
+const loginForm = document.getElementById('login-form');
+const usernameInput = document.getElementById('username');
+const passwordInput = document.getElementById('password');
+const loginButton = document.getElementById('do-login');
+
+// Add a login event listener
+loginButton.addEventListener('click', function () {
+  // Get the entered username and password
+  const enteredUsername = usernameInput.value;
+  const enteredPassword = passwordInput.value;
+
+  // Check if the username and password are correct
+  if (enteredUsername === 'admin' && enteredPassword === 'admin') {
+    // Redirect to the index page (you can replace 'index.html' with your actual index page URL)
+    window.location.href = 'index.html';
+  } else {
+    // Display an error message or handle invalid login here
+    alert('Invalid username or password. Please try again.');
+  }
+});
